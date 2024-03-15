4788e5719763e6dd9bcec08065f2b3f71540d910
oguzhantugrulakcay <oguzhantugrulakcay@hotmail.com>
2024-03-16 00:22:56 +0300

    added template

--- obj/Debug/net8.0/mygallery.AssemblyInfo.cs
+++ obj/Debug/net8.0/mygallery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mygallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a34dfaf4585726328afd15335d8526ad77becd")]
 [assembly: System.Reflection.AssemblyProductAttribute("mygallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mygallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
